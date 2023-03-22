414ebd3fae48d23aa9a8f98d56595778b065215e
권 도윤 <doyoun8813@i-commtech.com>
2023-03-23 08:24:13 +0900

    메인 메서드 args 인자값으로 속성 설정

--- src/main/java/hello/ExternalApplication.java
+++ src/main/java/hello/ExternalApplication.java
@@ -9,6 +9,10 @@ public class ExternalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExternalApplication.class, args);
         JavaSystem.print();
+
+        for (String arg : args) {
+            System.out.println("arg " + arg);
+        }
     }
 
 }
